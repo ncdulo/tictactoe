a981f4dcf1291a56075e028f7be39fc454b95505
Nick Cardullo <nick.cardullo@gmail.com>
2011-08-05 10:35:33 -0400

    Added in some comments

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -5,27 +5,33 @@
 #include <iostream>
 using namespace std;
 
+// Draw an empty row on the game board
 void GameBoard::drawEmpty() {
-	GameBoard::drawRow();		// Draw the top row now
+	GameBoard::drawRow();		// Draw the top row now, everything else loops
 	for(int i = 0; i < 3; i++) {
 		GameBoard::drawMiddleRow();
 		GameBoard::drawRow();
 	}
 }
 
+// TODO: Figure out the best way to implement and make it happen
 void GameBoard::drawWithMarks(vector<int> X, vector<int> O) {
 
 }
 
+// Draw a single, solid row on the board
 void GameBoard::drawRow() {
 	cout << "  +---+---+---+" << endl;
 	
 }
 
+// Draw a row with blanks for players to mark
 void GameBoard::drawMiddleRow() {
 	cout << "  |   |   |   |" << endl;
 }
 
+// This function is here only for testing
+// Will be moved into a Game class
 int main() {
 	GameBoard gb;
 	gb.drawEmpty();
